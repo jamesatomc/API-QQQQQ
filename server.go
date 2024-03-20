dbbca3475668dde3e3043c984398e4a05c06ea4f
jamesatomc <jiraphonwansai2002@gmail.com>
2024-03-20 23:50:10 +0700

    first commit

---
+++ server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jamesatomc/go-api/controllers"
+	"github.com/jamesatomc/go-api/models"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	loadEnv()
+    loadDatabase()
+    serveApplication()
+	
+}
+
+
+func loadEnv() {
+    err := godotenv.Load(".env.local")
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+}
+
+func loadDatabase() {
+    models.ConnectDatabase()
+	models.Database.AutoMigrate(&models.User{})
+}
+
+
+func serveApplication() {	
+
+	server := gin.Default()
+
+	models.ConnectDatabase()
+	
+	// User
+	server.GET("/users", controllers.FindUsers)
+	server.POST("/users", controllers.CreateUser)
+	server.GET("/users/:id", controllers.FindUser)
+	server.PATCH("/users/:id", controllers.UpdateUser)
+	server.DELETE("/users/:id", controllers.DeleteUser)
+	
+
+	server.Run()
+}
+
