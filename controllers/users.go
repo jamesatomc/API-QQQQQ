d69de08523187e5593db5f7573f855f27a202619
jamesatomc <jiraphonwansai2002@gmail.com>
2024-03-24 01:57:58 +0700

    app api aglo argon2id 11

--- controllers/users.go
+++ controllers/users.go
@@ -2,6 +2,7 @@ package controllers
 
 // Import necessary packages
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"strings"
@@ -235,44 +236,44 @@ func GenerateToken(userID uint, expiration time.Duration) (string, error) {
     return tokenString, nil
 }
 
-// // AuthenticationMiddleware function
-// func AuthenticationMiddleware() gin.HandlerFunc {
-//     return func(c *gin.Context) {
-//         // Retrieve token from cookie
-//         tokenString, err := c.Cookie("auth_token")
-//         if err != nil || tokenString == "" {
-//             c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-//             c.Abort()
-//             return
-//         }
+// AuthenticationMiddleware function
+func AuthenticationMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        // Retrieve token from cookie
+        tokenString, err := c.Cookie("auth_token")
+        if err != nil || tokenString == "" {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+            c.Abort()
+            return
+        }
 
-//         // Validate token
-//         token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-//             if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-//                 return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-//             }
-//             return []byte(os.Getenv("JWT_SECRET_KEY")), nil
-//         })
+        // Validate token
+        token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+            if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+                return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+            }
+            return []byte(os.Getenv("JWT_SECRET_KEY")), nil
+        })
 
-//         if err != nil {
-//             c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
-//             c.Abort()
-//             return
-//         }
+        if err != nil {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+            c.Abort()
+            return
+        }
 
-//         claims, ok := token.Claims.(jwt.MapClaims)
-//         if !ok || !token.Valid {
-//             c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-//             c.Abort()
-//             return
-//         }
+        claims, ok := token.Claims.(jwt.MapClaims)
+        if !ok || !token.Valid {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+            c.Abort()
+            return
+        }
 
-//         userID := uint(claims["user_id"].(float64)) 
-//         c.Set("user_id", userID) // Set the user ID in the Gin context
+        userID := uint(claims["user_id"].(float64)) 
+        c.Set("user_id", userID) // Set the user ID in the Gin context
 
-//         c.Next() 
-//     }
-// }
+        c.Next() 
+    }
+}
 
 // DeleteUser function
 func DeleteUser(c *gin.Context) {
