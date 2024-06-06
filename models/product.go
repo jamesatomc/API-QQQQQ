30139d24a13ae90dcaaa76ab06063922b7e560af
james atomc <56755216+jamesatomc@users.noreply.github.com>
2024-06-06 12:47:33 +0700

    Update product.go

--- models/product.go
+++ models/product.go
@@ -8,11 +8,11 @@ type Product struct {
     gorm.Model
     Name        string  `json:"name" binding:"required"`
     Description string  `json:"description" binding:"required"`
-    Price       float64 `json:"price" binding:"required"`
+    Price       string `json:"price" binding:"required"`
 }
 
 type UpdateProductInput struct {
     Name        string  `json:"name"`
     Description string  `json:"description"`
-    Price       float64 `json:"price"`
+    Price       string `json:"price"`
 }
