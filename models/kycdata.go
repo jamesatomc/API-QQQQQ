f13e1c7de4c19d3d069165f0678dddcaada4b113
james atomc <56755216+jamesatomc@users.noreply.github.com>
2024-06-06 12:48:26 +0700

    Update kycdata.go

--- models/kycdata.go
+++ models/kycdata.go
@@ -8,10 +8,10 @@ import (
 type KycData struct {
     gorm.Model
     FirstName 	 string 	`json:"firstname" binding:"required"`
-	LastName  	 string 	`json:"lastname" binding:"required"`
+    LastName  	 string 	`json:"lastname" binding:"required"`
     IdentityCard string     `json:"identitycard" binding:"required"`
     Country      string    `json:"country" binding:"required"`
     Address      string    `json:"address" binding:"required"`
     IDCardImage  string    `json:"idcardimage" binding:"required"`
-    UserID   uint
+    UserID   	 uint
 }
